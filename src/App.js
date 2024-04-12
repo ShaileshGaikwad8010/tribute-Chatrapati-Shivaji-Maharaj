3556ff87a408c84710fa6a5585b3bfc1d36d6531
ShaileshGaikwad8010 <121032736+ShaileshGaikwad8010@users.noreply.github.com>
2024-04-12 15:16:26 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import './App.css';
+import './style.css';
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import English from './components/English';
+import Hindi from './components/Hindi';
+import Marathi from './components/Marathi';
+import Telugu from './components/Telugu';
+import Kannada from './components/Kannada';
+import Sansrit from './components/Sansrit';
+import Footer from './components/Footer';
+import { MDBBtn, MDBContainer } from 'mdb-react-ui-kit';
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Routes>
+                <Route exact path="/" element={<English />} />
+                <Route exact path="/hindi" element={<Hindi />} />
+                <Route exact path="/marathi" element={<Marathi />} />
+                <Route exact path="/telugu" element={<Telugu />} />
+                <Route exact path="/kannada" element={<Kannada />} />
+                <Route exact path="/sanskrit" element={<Sansrit />} />
+        </Routes>
+      </Router>
+      <Footer/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <>
+    <footer class="footer">
+        <div class="waves">
+        <div class="wave" id="wave1"></div>
+        <div class="wave" id="wave2"></div>
+        <div class="wave" id="wave3"></div>
+        <div class="wave" id="wave4"></div>
+        </div>
+        <ul class="menu">
+        </ul>
+        <p>Created as a Tribute Page Project by <a href="https://github.com/ShaileshGaikwad8010" target='_blank' rel='noreferrer'>Shailesh Gaikwad</a></p>
+        <p>&copy;April 2024 <a href="https://www.linkedin.com/in/shailesh8010/" target='_blank' rel='noreferrer'>Shailesh Gaikwad</a></p>
+    </footer>
+    </>
+  )
+}
